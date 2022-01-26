a4b1f81aedfea383aad637f643a365c4cf1c6b49
신정아 <shinjeonga@twentyoz.kr>
2022-01-26 16:18:08 +0900

    init

---
+++ start-spring/src/test/java/jeong/startspring/StartSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jeong.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
